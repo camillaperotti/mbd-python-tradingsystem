37b9ca920997d2c3742f1428dba0c39842c9c311
camillaperotti <camilla.perotti@hotmail.com>
2025-03-04 14:37:13 +0100

    ETL py progress

--- Scripts/ETL.py
+++ Scripts/ETL.py
@@ -17,12 +17,31 @@ def process_data(prices_bruker):
     prices_bruker["Date"] = pd.to_datetime(prices_bruker.Date, format="%Y-%m-%d")
 
     # Return prices_bruker df
+    return prices_bruker
     
-def save_data(prices_)
+def save_data(prices_bruker, output_filepath):
+    prices_bruker.to_csv(output_filepath)
+    print(f"Processed data saved to {output_filepath}")
+
+def etl_pipeline(filepath, output_filepath):
+    # Extract
+    prices_bruker = load_data(filepath)
+    print(f"Data loaded from {filepath}")
+    
+    # Transform
+    prices_bruker = process_data(prices_bruker)
+    print("Data processed successfully")
+    
+    # Load
+    save_data(prices_bruker, output_filepath)
+
+    return prices_bruker  # Return the processed DataFrame
 
 
 if __name__ == "__main__":
     # raw data file
-    filepath = "/Users/camillaperotti/Desktop/IE/Courses MBD/Term 2/PDA II/00_GroupProject/mbd-python-tradingsystem/ETL/pricesbruker_output.csv"
+    filepath = "/Users/camillaperotti/Desktop/IE/Courses MBD/Term 2/PDA II/00_GroupProject/mbd-python-tradingsystem/ETL/data/us-shareprices-daily.csv"
     # clean data file
-    output_file = 
+    output_filepath = "/Users/camillaperotti/Desktop/IE/Courses MBD/Term 2/PDA II/00_GroupProject/mbd-python-tradingsystem/ETL/pricesbruker_output.csv"
+
+etl_pipeline(filepath, output_filepath)
