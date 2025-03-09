b6e7787d7deae58b6638878a694e6f23c36251ba
camillaperotti <camilla.perotti@hotmail.com>
2025-03-09 14:07:49 +0100

    progress

--- Scripts/run.py
+++ Scripts/run.py
@@ -19,67 +19,67 @@ class Company:
     # methods
     def load_data(self, filepath):
         # Load data
-        prices = pd.read_csv(filepath,delimiter=';')
+        prices_all = pd.read_csv(filepath,delimiter=';')
 
-        # Select BRUKER company data
-        prices_bruker = prices[prices["SimFinId"] == 1253240]
+        # Select 5 companies' price data
+        prices = prices_all[prices_all["SimFinId"].isin([1253240, 111052, 63877,56317, 59265])]
 
         # Return prices_bruker df
-        return prices_bruker
+        return prices
 
-    def process_data(self, prices_bruker):
+    def process_data(self, prices):
          # Impute missing values
-        prices_bruker["Dividend"] = prices_bruker["Dividend"].fillna(0)
+        prices["Dividend"] = prices["Dividend"].fillna(0)
 
         # Transform to correct data types
-        prices_bruker["Shares Outstanding"] =  prices_bruker["Shares Outstanding"].astype(int)
-        prices_bruker["Date"] = pd.to_datetime(prices_bruker.Date, format="%Y-%m-%d")
+        prices["Shares Outstanding"] =  prices["Shares Outstanding"].astype(int)
+        prices["Date"] = pd.to_datetime(prices.Date, format="%Y-%m-%d")
 
         # Return prices_bruker df
-        return prices_bruker
+        return prices
     
-    def save_data(self, prices_bruker, output_filepath):
-        prices_bruker.to_csv(output_filepath)
+    def save_data(self, prices, output_filepath):
+        prices.to_csv(output_filepath)
         print(f"Processed data saved to {output_filepath}")
 
     def etl_pipeline(self, filepath, output_filepath):
         # Extract
-        self.prices_bruker = self.load_data(filepath)
+        self.prices = self.load_data(filepath)
         print(f"Data loaded from {filepath}")
         
         # Transform
-        self.prices_bruker = self.process_data(self.prices_bruker)
+        self.prices = self.process_data(self.prices)
         print("Data processed successfully")
         
         # Load
-        self.save_data(self.prices_bruker, output_filepath)
+        self.save_data(self.prices, output_filepath)
 
-        return self.prices_bruker  # Return the processed DataFrame
+        return self.prices  # Return the processed DataFrame
 
 
-    def prepare_data(self, prices_bruker):
+    def prepare_data(self, prices):
         # Ensure data is sorted by date
-        prices_bruker = prices_bruker.sort_values("Date").reset_index(drop=True)
+        prices = prices.sort_values("Date").reset_index(drop=True)
 
         # Drop all unwanted columns, rename adjusted close column
-        prices_bruker.drop(columns=["Ticker","SimFinId","Open","High", "Low", "Close", "Volume", "Dividend","Shares Outstanding"],inplace = True)
-        prices_bruker.rename(columns={"Adj. Close": "Price"}, inplace= True)
+        prices.drop(columns=["Ticker","SimFinId","Open","High", "Low", "Close", "Volume", "Dividend","Shares Outstanding"],inplace = True)
+        prices.rename(columns={"Adj. Close": "Price"}, inplace= True)
 
         # Adding last 4 days' prices into the df
         days = 4
         for day in range(1, days + 1):
-            prices_bruker[f"Price d-{day}"] = prices_bruker["Price"].shift(day)
+            prices[f"Price d-{day}"] = prices["Price"].shift(day)
 
         #dropping first rows with missing values
-        prices_bruker = prices_bruker.dropna()
+        prices = prices.dropna()
 
         # Adding prediction
-        prices_bruker["Price_Up"] = (prices_bruker["Price"].shift(-1) > prices_bruker["Price"]).astype(int)
-        prices_bruker = prices_bruker.drop(index=1238)
+        prices["Price_Up"] = (prices["Price"].shift(-1) > prices["Price"]).astype(int)
+        prices = prices.drop(index=1238)
 
         # Splitting features from target
-        x = prices_bruker.drop(columns=["Price_Up", "Date"])
-        y = prices_bruker["Price_Up"]
+        x = prices.drop(columns=["Price_Up", "Date"])
+        y = prices["Price_Up"]
 
         # Splitting data into training and testing
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
@@ -90,7 +90,7 @@ class Company:
         x_test_scaled = sc.transform(x_test)
 
         # Returning all variables
-        return prices_bruker, x_train_scaled, x_test_scaled, y_train, y_test, sc
+        return prices, x_train_scaled, x_test_scaled, y_train, y_test, sc
 
     def train_model(self, x_train_scaled, y_train, model_path, sc):
         # Training model
@@ -112,12 +112,12 @@ class Company:
         print("Model and scaler loaded successfully.")
         return classifier, sc
 
-    def predict_next_day(self, prices_bruker, classifier, sc):
+    def predict_next_day(self, prices, classifier, sc):
         # Ensure "Price_Up" exists before dropping
-        if "Price_Up" in prices_bruker.columns:
-            latest_features = prices_bruker.iloc[-1:].drop(columns=["Price_Up", "Date"])
+        if "Price_Up" in prices.columns:
+            latest_features = prices.iloc[-1:].drop(columns=["Price_Up", "Date"])
         else:
-            latest_features = prices_bruker.iloc[-1:].drop(columns=["Date"]) 
+            latest_features = prices.iloc[-1:].drop(columns=["Date"]) 
 
         latest_features_scaled = sc.transform(latest_features)  # Scale it
 
@@ -134,7 +134,7 @@ class Company:
 
     def ml_pipeline(self, model_path):
         # Prepare data
-        prices_bruker, x_train_scaled, x_test_scaled, y_train, y_test, sc = self.prepare_data(self.prices_bruker)
+        prices, x_train_scaled, x_test_scaled, y_train, y_test, sc = self.prepare_data(self.prices)
 
         # Load model
         # Check if model already exists
@@ -148,14 +148,14 @@ class Company:
             classifier, sc = self.train_model(x_train_scaled, y_train, model_path, sc)
 
         # Predict next day's movement
-        prediction = self.predict_next_day(prices_bruker, classifier, sc)
+        prediction = self.predict_next_day(prices, classifier, sc)
         print(f"Next day's price movement: {prediction}")
 
 if __name__ == "__main__":
     # raw data file
     filepath = "/Users/camillaperotti/Desktop/IE/Courses MBD/Term 2/PDA II/00_GroupProject/mbd-python-tradingsystem/ETL/data/us-shareprices-daily.csv"
     # clean data file
-    output_filepath = "/Users/camillaperotti/Desktop/IE/Courses MBD/Term 2/PDA II/00_GroupProject/mbd-python-tradingsystem/ETL/pricesbruker_output.csv"
+    output_filepath = "/Users/camillaperotti/Desktop/IE/Courses MBD/Term 2/PDA II/00_GroupProject/mbd-python-tradingsystem/ETL/prices_output.csv"
 
      # model path as output
     model_path = "models/model.pkl"
