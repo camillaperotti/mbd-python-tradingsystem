e772c115832cf68873d9af09838f579341127e53
luciasarobe <luciasarbe@student.ie.edu>
2025-03-14 15:58:21 +0100

    req12

--- Scripts/API.py
+++ Scripts/API.py
@@ -6,7 +6,7 @@ import requests
 import logging
 import pandas as pd
 from datetime import datetime
-from Scripts.Exceptions import SimFinError, ResourceNotFoundError
+from Exceptions import SimFinError, ResourceNotFoundError
 
 
 #Define class PySimFin - main client for the SimFin wrapper
